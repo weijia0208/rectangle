ea592149d60e6e88733f065437cfcb930052bf91
weijia0208 <10024752762@qq.com>
2019-03-27 17:23:05 +0800

    rectangle v1.0

--- rectangle.js
+++ rectangle.js
@@ -3,11 +3,42 @@ $(function(){
       $height=$('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $area = $('#area');
+      $widthValidation = $('#width-validation'),
+      $heightValidation = $('#height-validation'),
+      $area = $('#area'),
+      isOk = false;
+  
+
+  $height.focusout(function(){
+    var result = validation($height.val());
+    isOk = result.isOK;
+    if(!result.isOK){
+      $heightValidation.html('高度'+result.reason);
+      $height.select();
+    }else{
+      $heightValidation.html('');
+    }
+  });
+
+  $width.focusout(function(){
+    var result = validation($width.val());
+    isOk = result.isOK;
+    if(!result.isOK){
+      $widthValidation.html('宽度'+result.reason);
+      $width.select();
+    }else{
+      $widthValidation.html('');               
+    }
+  });
+
+
   $btnCal.click(function(){
+    if(!isOk) return;
+
+
     var w = $width.val(),
         h = $height.val();
-
+    
     var rect = rectangle();
 
     $perimeter.val(rect.perimeter(w,h));
