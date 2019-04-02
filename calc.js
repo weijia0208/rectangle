58b28584805696394776724c347793b20fb1907c
weijia0208 <10024752762@qq.com>
2019-04-02 15:48:47 +0800

    feat-h5-validation

--- calc.js
+++ calc.js
@@ -14,25 +14,3 @@ function rectangle(){
 }
 
 
-function validation(data){
-  var result = {
-    isOK:false,
-    reason:''
-  };
-
-  if(data === ''){
-    result.reason = '不能为空';
-    return result;
-  }
-  if(Number(data) < 0){
-    result.reason = '不能为负';
-    return result;
-  }
-  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)){
-    result.reason = '数据格式不正确';
-    return result;
-  }
-  result.isOK = true;
-  return result;
-}
-
--- rectangle.js
+++ rectangle.js
@@ -1,41 +1,16 @@
 $(function(){
   var $width=$('#width'),
       $height=$('#height'),
-      $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $widthValidation = $('#width-validation'),
-      $heightValidation = $('#height-validation'),
-      $area = $('#area'),
-      isOk = false;
+      $form = $('form'),
+      $area = $('#area');
   
 
-  $height.focusout(function(){
-    var result = validation($height.val());
-    isOk = result.isOK;
-    if(!result.isOK){
-      $heightValidation.html('高度'+result.reason);
-      $height.select();
-    }else{
-      $heightValidation.html('');
-    }
-  });
+ 
 
-  $width.focusout(function(){
-    var result = validation($width.val());
-    isOk = result.isOK;
-    if(!result.isOK){
-      $widthValidation.html('宽度'+result.reason);
-      $width.select();
-    }else{
-      $widthValidation.html('');               
-    }
-  });
-
-
-  $btnCal.click(function(){
-    if(!isOk) return;
-
-
+  $form.submit(function(e){
+    e.preventDefault();
+   
     var w = $width.val(),
         h = $height.val();
     
