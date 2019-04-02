e838ecfac1dd9c30fc1a27bcdfc84835ffbbc187
weijia0208 <10024752762@qq.com>
2019-04-02 14:25:01 +0800

    feat-form-validatio

--- rectangle.js
+++ rectangle.js
@@ -7,32 +7,7 @@ $(function(){
       $heightValidation = $('#height-validation'),
       $area = $('#area'),
       isOk = false;
-  
-
-  $height.focusout(function(){
-    var result = validation($height.val());
-    isOk = result.isOK;
-    if(!result.isOK){
-      $heightValidation.html('高度'+result.reason);
-      $height.select();
-    }else{
-      $heightValidation.html('');
-    }
-  });
-
-  $width.focusout(function(){
-    var result = validation($width.val());
-    isOk = result.isOK;
-    if(!result.isOK){
-      $widthValidation.html('宽度'+result.reason);
-      $width.select();
-    }else{
-      $widthValidation.html('');               
-    }
-  });
-
-
-  $btnCal.click(function(){
+   $btnCal.click(function(){
     if(!isOk) return;
 
 
