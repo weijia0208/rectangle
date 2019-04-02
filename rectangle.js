e96fe6da002271727128ce43fdd68e7f0436900b
weijia0208 <10024752762@qq.com>
2019-04-02 16:09:09 +0800

    reactangle v1.0

--- rectangle.js
+++ rectangle.js
@@ -7,17 +7,38 @@ $(function(){
       $heightValidation = $('#height-validation'),
       $area = $('#area'),
       isOk = false;
-   $btnCal.click(function(){
-    if(!isOk) return;
+  
 
-
-    var w = $width.val(),
-        h = $height.val();
-    
-    var rect = rectangle();
-
-    $perimeter.val(rect.perimeter(w,h));
-    $area.val(rect.area(w,h));
+  $height.focusout(function(){
+    var result = validation($height.val());
+    isOk = result.isOK;
+    if(!result.isOK){
+      $heightValidation.html('高度'+result.reason);
+      $height.select();                      
+    }else{
+      $heightValidation.html('');                
+    }        
   });
 
+  $width.focusout(function(){
+    var result = validation($width.val());
+    isOk = result.isOK;
+    if(!result.isOK){
+      $widthValidation.html('宽度'+result.reason);
+      $width.select();                      
+    }else{
+      $widthValidation.html('');                               
+    }        
+  });
+
+
+  $btnCal.click(function(){
+    if(!isOk) return;
+    var w = $width.val(),
+        h = $height.val();
+    var rect = rectangle();
+    $perimeter.val(rect.perimeter(w,h));
+    $area.val(rect.area(w,h));          
+  });
 });
+
